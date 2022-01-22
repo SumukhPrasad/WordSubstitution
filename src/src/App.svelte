b4c6e4374e93696295732eeb58c59546b07ca48f
Sumukh Prasad <sumukhprasad@icloud.com>
2022-01-22 10:40:42 +0530

    Added base styling to body.

--- src/src/App.svelte
+++ src/src/App.svelte
@@ -1,30 +1,23 @@
 <script>
-	export let name;
+	
 </script>
 
+<svelte:head>
+	<style>
+		body {
+			max-width: 600px;
+			max-height: 300px;
+		}
+		</style>
+</svelte:head>
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>WordSubstitution</h1>
 </main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		position: absolute;
+		width: 100%;
+		height: 100%;
 	}
 </style>
